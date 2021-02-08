67b73b7a7b4873c85af86670c76cbd4817cddc20
Steve Kroll <steve.m.kroll@gmail.com>
2021-02-08 09:57:37 -0600

    added preview function

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"flag"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 // flags
@@ -23,7 +25,60 @@ func init() {
 
 // main
 func main() {
-	flag.StringVar(&fixFlag, "fix", "none", "defines the quick fix you would like to perform")
-	flag.Parse()
-	log.Printf("fix flag is...%+v", fixFlag)
+	// flag.StringVar(&fixFlag, "fix", "none", "defines the quick fix you would like to perform")
+	// flag.Parse()
+	// log.Printf("fix flag is...%+v", fixFlag)
+}
+
+// PreviewWarnings displays all the warnings that will be fixed
+func PreviewWarnings() {
+	var files []string
+	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
+		path = filepath.Clean(path)
+		dir, file := filepath.Split(path)
+		if !strings.HasPrefix(dir, ".") &&
+			!strings.HasPrefix(file, ".") &&
+			strings.Contains(file, ".go") {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	for i := range files {
+		warnings, err := getWarnings(files[i])
+		if err != nil {
+			panic(err)
+		}
+		if len(warnings) > 0 {
+			for i := range warnings {
+				log.Println(warnings[i].Text)
+			}
+		}
+	}
+}
+
+// FixAll fixes all supported warnings
+func FixAll() {
+	var files []string
+	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
+		path = filepath.Clean(path)
+		dir, file := filepath.Split(path)
+		if !strings.HasPrefix(dir, ".") &&
+			!strings.HasPrefix(file, ".") &&
+			strings.Contains(file, ".go") {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	for i := range files {
+		err := fixFile(files[i])
+		if err != nil {
+			panic(err)
+		}
+	}
 }
--- main_test.go
+++ main_test.go
@@ -1,31 +1,36 @@
 package main
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
 	"testing"
 )
 
-func TestMain(t *testing.T) {
-	var files []string
-	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
-		path = filepath.Clean(path)
-		dir, file := filepath.Split(path)
-		if !strings.HasPrefix(dir, ".") &&
-			!strings.HasPrefix(file, ".") &&
-			strings.Contains(file, ".go") {
-			files = append(files, path)
-		}
-		return nil
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-	for i := range files {
-		err := fixFile(files[i])
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
+// func TestMain(t *testing.T) {
+// 	var files []string
+// 	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
+// 		path = filepath.Clean(path)
+// 		dir, file := filepath.Split(path)
+// 		if !strings.HasPrefix(dir, ".") &&
+// 			!strings.HasPrefix(file, ".") &&
+// 			strings.Contains(file, ".go") {
+// 			files = append(files, path)
+// 		}
+// 		return nil
+// 	})
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
+// 	for i := range files {
+// 		err := fixFile(files[i])
+// 		if err != nil {
+// 			t.Fatal(err)
+// 		}
+// 	}
+// }
+
+func TestFixAll(t *testing.T) {
+	FixAll()
+}
+
+func TestPreviewWarnings(t *testing.T) {
+	PreviewWarnings()
 }
