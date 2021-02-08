a11f73d8c6b1c59a5ef7877770f320c19715f4c7
Steve Kroll <steve.m.kroll@gmail.com>
2021-02-08 10:06:05 -0600

    updated package name

--- quickfix.go
+++ quickfix.go
@@ -1,4 +1,4 @@
-package main
+package quickfix
 
 import (
 	"log"
@@ -6,29 +6,6 @@ import (
 	"path/filepath"
 	"strings"
 )
-
-// flags
-var fixFlag string
-
-// strings
-var (
-	fixNone    string = "none"
-	fixAll     string = "all"
-	fixStructs string = "structs"
-)
-
-// init
-func init() {
-	// flag.StringVar(&fixFlag, "fix", "none", "defines the quick fix you would like to perform")
-	// flag.Parse()
-}
-
-// main
-func main() {
-	// flag.StringVar(&fixFlag, "fix", "none", "defines the quick fix you would like to perform")
-	// flag.Parse()
-	// log.Printf("fix flag is...%+v", fixFlag)
-}
 
 // PreviewWarnings displays all the warnings that will be fixed
 func PreviewWarnings() {
